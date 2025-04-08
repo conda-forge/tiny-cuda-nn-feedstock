0768c768271f6857ead8ec7dfacd253149296775
Jeongseok (JS) Lee <142548112+jeongseok-meta@users.noreply.github.com>
2025-04-08 11:21:18 -0700

    export CUDA_HOME=$PREFIX

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,8 @@
 
 set -exo pipefail
 
-export TCNN_CUDA_ARCHITECTURES=86,89,100,120
+export TCNN_CUDA_ARCHITECTURES=86,89
+export CUDA_HOME=$PREFIX
 
 cd bindings/torch
 $PYTHON -m pip install . -vv --no-deps --no-build-isolation
