bfef75d068b2dbf5e25379750437725d5253ada0
Jeongseok (JS) Lee <142548112+jeongseok-meta@users.noreply.github.com>
2025-04-08 11:09:28 -0700

    Update TCNN_CUDA_ARCHITECTURES

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,7 @@
 
 set -exo pipefail
 
-export TCNN_CUDA_ARCHITECTURES=89
+export TCNN_CUDA_ARCHITECTURES=86,89,100,120
 
 cd bindings/torch
 $PYTHON -m pip install . -vv --no-deps --no-build-isolation
