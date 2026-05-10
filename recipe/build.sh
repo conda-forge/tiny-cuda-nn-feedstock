ec64f7dca85668641e2bea3624e2ee3ca8c0d742
Jeongseok Lee <jeongseok@meta.com>
2026-05-09 18:11:03 -0700

    Fix CUDA 13 build environment

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,8 +3,9 @@
 set -exo pipefail
 
 export TCNN_CUDA_ARCHITECTURES=75,80,86,90,120
-export CUDA_HOME=$PREFIX
+export CUDA_HOME=$BUILD_PREFIX
 export CMAKE_PREFIX_PATH=$PREFIX
+export MAX_JOBS=${MAX_JOBS:-1}
 
 cd bindings/torch
 $PYTHON -m pip install . -vv
